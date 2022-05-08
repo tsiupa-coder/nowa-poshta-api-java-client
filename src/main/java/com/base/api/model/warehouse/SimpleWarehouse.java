135bf6e1fca6ad8c9ca355be0740791b22d03c69
Ivan tsiupa <ivan.tsiupa@inventorsoft.co>
2022-05-08 13:10:18 +0300

    methods get

--- src/main/java/com/base/api/model/warehouse/SimpleWarehouse.java
+++ src/main/java/com/base/api/model/warehouse/SimpleWarehouse.java
@@ -7,5 +7,14 @@ import lombok.experimental.FieldDefaults;
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class SimpleWarehouse {kjkuoo
+public class SimpleWarehouse {
+
+    String CityName;
+    String CityRef;
+    String Language;
+    String TypeOfWarehouseRef;
+    String WarehouseId;
+
+
+
 }
