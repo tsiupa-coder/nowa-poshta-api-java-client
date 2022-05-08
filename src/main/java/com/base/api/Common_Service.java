9c5a8e5d541ffd8ac284df61149804f14b9263dd
ivan.tsiupa <ivan.tsupa111@gmail.com>
2022-05-08 17:47:29 +0300

    full imp common endpoints

--- src/main/java/com/base/api/Common_Service.java
+++ src/main/java/com/base/api/Common_Service.java
@@ -1,9 +1,18 @@
 package com.base.api;
 
-import com.base.api.model.addresses.address.SimpleAddress;
+import com.base.api.model.common.SimplePack;
 import com.base.api.model.common.SimpleTimeInterval;
+import com.base.api.request.Pagination;
+import com.base.api.response.common.GetCargoDescriptionList;
 import com.base.api.response.common.GetCargoTypes;
+import com.base.api.response.common.GetMessageCodeText;
+import com.base.api.response.common.GetOwnershipFormsList;
+import com.base.api.response.common.GetPackList;
+import com.base.api.response.common.GetPalletsList;
+import com.base.api.response.common.GetServiceTypes;
 import com.base.api.response.common.GetTimeIntervals;
+import com.base.api.response.common.GetTiresWheelsList;
+import com.base.api.response.common.GetTypesOfPayersForRedelivery;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
@@ -36,7 +45,7 @@ public class Common_Service {
     }
 
 
-    public GetCargoTypes getCargoTypes( String api_key) throws IOException {
+    public GetCargoTypes getCargoTypes(String api_key) throws IOException {
 
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("apiKey", api_key);
@@ -50,6 +59,158 @@ public class Common_Service {
         return http_helper
                 .postRequest(URL, jsonObject)
                 .getResponse(new GetCargoTypes());
+    }
+
+    public GetCargoTypes getBackwardDeliveryCargoTypes(String api_key) throws IOException {
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("apiKey", api_key);
+        jsonObject.addProperty("modelName", "Common");
+        jsonObject.addProperty("calledMethod", "getBackwardDeliveryCargoTypes");
+
+        JsonObject jsonObjectIn = new JsonObject();
+
+        jsonObject.add("methodProperties", jsonObjectIn);
+
+        return http_helper
+                .postRequest(URL, jsonObject)
+                .getResponse(new GetCargoTypes());
+    }
+
+    public GetPalletsList getPalletsList(String api_key) throws IOException {
+
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("apiKey", api_key);
+        jsonObject.addProperty("modelName", "Common");
+        jsonObject.addProperty("calledMethod", "getPalletsList");
+
+        JsonObject jsonObjectIn = new JsonObject();
+
+        jsonObject.add("methodProperties", jsonObjectIn);
+
+        return http_helper
+                .postRequest(URL, jsonObject)
+                .getResponse(new GetPalletsList());
+    }
+
+    public GetTypesOfPayersForRedelivery getTypesOfPayersForRedelivery(String api_key) throws IOException {
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("apiKey", api_key);
+        jsonObject.addProperty("modelName", "Common");
+        jsonObject.addProperty("calledMethod", "getTypesOfPayersForRedelivery");
+
+        JsonObject jsonObjectIn = new JsonObject();
+
+        jsonObject.add("methodProperties", jsonObjectIn);
+
+        return http_helper
+                .postRequest(URL, jsonObject)
+                .getResponse(new GetTypesOfPayersForRedelivery());
+    }
+
+    public GetPackList getPackList(SimplePack pack, String api_key) throws IOException {
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("apiKey", api_key);
+        jsonObject.addProperty("modelName", "Common");
+        jsonObject.addProperty("calledMethod", "getPackList");
+
+        JsonObject jsonObjectIn = (JsonObject) new Gson().toJsonTree(pack);
+
+        jsonObject.add("methodProperties", jsonObjectIn);
+
+        return http_helper
+                .postRequest(URL, jsonObject)
+                .getResponse(new GetPackList());
+    }
+
+    public GetTiresWheelsList getTiresWheelsList(String api_key) throws IOException {
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("apiKey", api_key);
+        jsonObject.addProperty("modelName", "Common");
+        jsonObject.addProperty("calledMethod", "getTiresWheelsList");
+
+        JsonObject jsonObjectIn = new JsonObject();
+
+        jsonObject.add("methodProperties", jsonObjectIn);
+
+        return http_helper
+                .postRequest(URL, jsonObject)
+                .getResponse(new GetTiresWheelsList());
+    }
+
+    public GetCargoDescriptionList getCargoDescriptionList(String findByString, Pagination pagination, String api_key) throws IOException {
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("apiKey", api_key);
+        jsonObject.addProperty("modelName", "Common");
+        jsonObject.addProperty("calledMethod", "getCargoDescriptionList");
+
+        JsonObject jsonObjectIn = new JsonObject();
+        jsonObjectIn.addProperty("FindByString", findByString);
+        jsonObjectIn.addProperty("Page", pagination.getPage());
+
+        jsonObject.add("methodProperties", jsonObjectIn);
+
+        return http_helper
+                .postRequest(URL, jsonObject)
+                .getResponse(new GetCargoDescriptionList());
+    }
+
+    public GetMessageCodeText getMessageCodeText(String api_key) throws IOException {
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("apiKey", api_key);
+        jsonObject.addProperty("modelName", "Common");
+        jsonObject.addProperty("calledMethod", "getMessageCodeText");
+
+        JsonObject jsonObjectIn = new JsonObject();
+
+        jsonObject.add("methodProperties", jsonObjectIn);
+
+        return http_helper
+                .postRequest(URL, jsonObject)
+                .getResponse(new GetMessageCodeText());
+    }
+
+
+
+    public GetServiceTypes getServiceTypes(String api_key) throws IOException {
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("apiKey", api_key);
+        jsonObject.addProperty("modelName", "Common");
+        jsonObject.addProperty("calledMethod", "getServiceTypes");
+
+        JsonObject jsonObjectIn = new JsonObject();
+
+        jsonObject.add("methodProperties", jsonObjectIn);
+
+        return http_helper
+                .postRequest(URL, jsonObject)
+                .getResponse(new GetServiceTypes());
+    }
+
+    public GetOwnershipFormsList getOwnershipFormsList(String api_key) throws IOException {
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("apiKey", api_key);
+        jsonObject.addProperty("modelName", "Common");
+        jsonObject.addProperty("calledMethod", "getOwnershipFormsList");
+
+        JsonObject jsonObjectIn = new JsonObject();
+
+        jsonObject.add("methodProperties", jsonObjectIn);
+
+        return http_helper
+                .postRequest(URL, jsonObject)
+                .getResponse(new GetOwnershipFormsList());
 
     }
 }
+
+
+
