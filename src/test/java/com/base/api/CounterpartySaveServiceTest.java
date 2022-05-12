12537d9845c646285e62fdcbde9065281afaf2f4
ivan.tsiupa <ivan.tsupa111@gmail.com>
2022-05-12 14:57:57 +0300

    countryparty save tests

---
+++ src/test/java/com/base/api/CounterpartySaveServiceTest.java
@@ -0,0 +1,20 @@
+package com.base.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CounterpartySaveServiceTest {
+
+    @Test
+    void createCounterparty() {
+    }
+
+    @Test
+    void testCreateCounterparty() {
+    }
+
+    @Test
+    void testCreateCounterparty1() {
+    }
+}
