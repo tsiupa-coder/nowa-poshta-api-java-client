24b94456509abdbc273b8b2fa79a9676bd5363fc
ivan.tsiupa <ivan.tsupa111@gmail.com>
2022-05-13 17:53:53 +0300

    CounterpartyServiceTest

---
+++ src/test/java/com/base/api/CounterpartyServiceTest.java
@@ -0,0 +1,16 @@
+package com.base.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CounterpartyServiceTest {
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteCounterparty() {
+    }
+}
