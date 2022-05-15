f2c6b6253ecd26bc0aff05e7e8b293fce822ab28
ivan.tsiupa <ivan.tsupa111@gmail.com>
2022-05-15 17:07:54 +0300

    general body ContactPersonService

--- src/main/java/com/base/api/ContactPersonService.java
+++ src/main/java/com/base/api/ContactPersonService.java
@@ -14,14 +14,7 @@ public class ContactPersonService extends Service {
 
     public CreateContactPerson create(SimpleContactPerson person, String api_key) throws IOException {
 
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("apiKey", api_key);
-        jsonObject.addProperty("modelName", "ContactPerson");
-        jsonObject.addProperty("calledMethod", "save");
-
-        JsonObject jsonObjectIn = (JsonObject) new Gson().toJsonTree(person);
-
-        jsonObject.add("methodProperties", jsonObjectIn);
+        JsonObject jsonObject = jsonService.toObject(person, api_key, "ContactPerson", "save");
 
         return service
                 .postRequest(URL, jsonObject)
@@ -31,14 +24,7 @@ public class ContactPersonService extends Service {
 
     public CreateContactPerson update(PersonData person, String api_key) throws IOException {
 
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("apiKey", api_key);
-        jsonObject.addProperty("modelName", "ContactPerson");
-        jsonObject.addProperty("calledMethod", "update");
-
-        JsonObject jsonObjectIn = (JsonObject) new Gson().toJsonTree(person);
-
-        jsonObject.add("methodProperties", jsonObjectIn);
+        JsonObject jsonObject = jsonService.toObject(person, api_key, "ContactPerson", "update");
 
         return service
                 .postRequest(URL, jsonObject)
@@ -47,21 +33,13 @@ public class ContactPersonService extends Service {
     }
 
     public DeleteContactPerson deleteContactPerson(String ref, String api_key) throws IOException {
-
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("apiKey", api_key);
-        jsonObject.addProperty("modelName", "ContactPerson");
-        jsonObject.addProperty("calledMethod", "delete");
-
         JsonObject jsonObjectIn = new JsonObject();
         jsonObjectIn.addProperty("Ref", ref);
 
-        jsonObject.add("methodProperties", jsonObjectIn);
-
+        JsonObject jsonObject = jsonService.toObject(api_key, "ContactPerson", "delete", jsonObjectIn);
         return service
                 .postRequest(URL, jsonObject)
                 .getResponse(new DeleteContactPerson());
-
 
     }
 }
