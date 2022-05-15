87f4e4d24490adcdef94bccf0a7e20876f8f0e24
ivan.tsiupa <ivan.tsupa111@gmail.com>
2022-05-15 14:21:52 +0300

    file service test

---
+++ src/test/java/com/base/api/FileServiceTest.java
@@ -0,0 +1,16 @@
+package com.base.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileServiceTest {
+
+    @Test
+    void print() {
+    }
+
+    @Test
+    void testPrint() {
+    }
+}
