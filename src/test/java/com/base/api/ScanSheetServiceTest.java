4da749068a827086c3a7f26add8af951b536ad4f
ivan.tsiupa <ivan.tsupa111@gmail.com>
2022-05-15 15:14:39 +0300

    tests ScanSheet

---
+++ src/test/java/com/base/api/ScanSheetServiceTest.java
@@ -0,0 +1,28 @@
+package com.base.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScanSheetServiceTest {
+
+    @Test
+    void getScanSheet() {
+    }
+
+    @Test
+    void insertDocuments() {
+    }
+
+    @Test
+    void getScanSheetList() {
+    }
+
+    @Test
+    void deleteScanSheet() {
+    }
+
+    @Test
+    void removeDocuments() {
+    }
+}
