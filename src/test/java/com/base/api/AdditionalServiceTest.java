227f8c8c86553ce09fb0bbf91dc2baab99260ee4
ivan.tsiupa <ivan.tsupa111@gmail.com>
2022-05-19 09:32:33 +0300

    AdditionalServiceTest

---
+++ src/test/java/com/base/api/AdditionalServiceTest.java
@@ -0,0 +1,19 @@
+package com.base.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdditionalServiceTest {
+
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void checkPossibilityCreateReturn() {
+    }
+}
