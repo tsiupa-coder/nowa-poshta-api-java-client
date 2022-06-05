8027cb7294ddc8681d63b2782872d9380b71216e
ivan.tsiupa <ivan.tsupa111@gmail.com>
2022-06-05 13:30:30 +0300

    testGetCargoTypes  test method

--- src/test/java/com/base/api/Common_ServiceTest.java
+++ src/test/java/com/base/api/Common_ServiceTest.java
@@ -1,13 +1,24 @@
 package com.base.api;
 
+import com.base.api.mainservice.Common_Service;
+import com.base.api.response.info.common.GetCargoTypes;
 import junit.framework.TestCase;
+
+import java.io.IOException;
 
 public class Common_ServiceTest extends TestCase {
 
+    Common_Service service = new Common_Service();
+    private final String API_KEY = "64db4746458023076ed38c878a0b398f";
     public void testGetTimeIntervals() {
+
     }
 
-    public void testGetCargoTypes() {
+    public void testGetCargoTypes() throws IOException {
+        GetCargoTypes info =  service.getCargoTypes(API_KEY);
+
+        System.out.println(info.getStatusResponse());
+        info.getData().forEach(System.out::println);
     }
 
     public void testGetBackwardDeliveryCargoTypes() {
